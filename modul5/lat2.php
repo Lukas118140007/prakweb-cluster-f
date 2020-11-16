8402aafb6efeb08b96891d9413a99c13c2583c6d
Lukas118140007 <65351665+Lukas118140007@users.noreply.github.com>
2020-11-16 11:01:58 +0700

    updateMinggu2

---
+++ modul5/lat2.php
@@ -0,0 +1,8 @@
+<?php
+    $kata = array("lanirne","aduh","qifuat","toda","anevi","samid",
+    "kifuat","januardo","soviet","zidan");
+    sort($kata);
+    for($i=0;$i<10;$i++){
+        echo "$kata[$i] <br>";
+    }
+?>
